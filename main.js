bf3133d447a192865ed08a036413c349c0bebce7
Rebtand <u.ryantoo@gmail.com>
2015-04-30 14:40:36 +1000

    loading a level

--- main.js
+++ main.js
@@ -31,6 +31,15 @@ function getDeltaTime()
 
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
+var LAYER_COUNT = 5;
+var MAP = {tw: 61, th: 15};
+var TILE = 32;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 0;
+var TILESET_SPACING = 0;
+var TILESET_COUNT_X = 8;
+var TILESET_COUNT_Y = 18;
+var tileset = document.createElement("img");
 
 
 // some variables to calculate the Frames Per Second (FPS - this tells use
@@ -45,7 +54,32 @@ var fpsTime = 0;
 var player = new Player();
 var keyboard = new Keyboard();
 var enemy = new Enemy();
+tileset.src = "assets/generictiles.png";
 
+function drawMap()
+{
+	 for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+	 {
+		 var idx = 0;
+		 for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+		 {
+			 for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+			 {
+				 if( level1.layers[layerIdx].data[idx] != 0 )
+				 {
+					 // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+					 // correct tile
+					 var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+					 var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+					 var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+					 context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+				}
+			idx++;
+			}
+		}
+	}
+}
+ 
 function run()
 {
 	context.fillStyle = "#ccc";		
